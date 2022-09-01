bdf8af71892fe1c3727aa98a9f95e80406890d13
Tristen <tristen.hammon21@gmail.com>
2022-09-01 15:38:16 -0500

    commit tree 2014a93a85426cfd1d7aa0fb155a4dd22ec4dd4d

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("The DOM has loaded")
+    console.log(document.querySelector('p')
+    .textContent = ('This is really cool!'))
+});
+
